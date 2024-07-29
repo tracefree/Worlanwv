75252172debfdf093b5160a9f83c7ad1de785809
Rie <riedev@proton.me>
2024-07-29 16:36:00 +0200

    Fix (?) shader padding

--- src/ui/palette.rs
+++ src/ui/palette.rs
@@ -7,4 +7,4 @@ pub const BUTTON_TEXT: Color = Color::srgb(0.925, 0.925, 0.925);
 pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
 pub const HEADER_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
 
-pub const NODE_BACKGROUND: Color = Color::srgba(0.2, 0.2, 0.2, 0.9);
+pub const NODE_BACKGROUND: Color = Color::srgba(0.05, 0.05, 0.05, 0.9);
